e3a2144121f364eb55c145b5ba8502ff65be2a9e
Joost de Folter <folterj@gmail.com>
2023-12-23 10:36:17 +0100

    Tidy up, fix simple testing

--- src/napari_mass/debug.py
+++ src/napari_mass/debug.py
@@ -3,11 +3,6 @@ from napari import Viewer, run
 viewer = Viewer()
 
 dock_widget, plugin_widget = viewer.window.add_plugin_dock_widget('napari-mass')
-#dock_widget, plugin_widget = viewer.window.add_plugin_dock_widget('napari-mass', 'Start Microscopy Array Section Setup')
-
-# Click some buttons to save time
-#plugin_widget.subwidgets["task"].task_buttons["mito"].click()
-#plugin_widget.subwidgets["nxf"].nxf_profile_box.setCurrentIndex(1)
-#plugin_widget.subwidgets["nxf"].overwrite_btn.click()
+#dock_widget, plugin_widget = viewer.window.add_plugin_dock_widget('napari-mass', 'Microscopy Array Section Setup')
 
 run()
